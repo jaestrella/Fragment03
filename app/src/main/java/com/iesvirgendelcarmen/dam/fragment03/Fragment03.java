783c2609b637c4cff672d6c644c21183ab989f06
jaestrella <jaestrella1991@gmail.com>
2017-10-06 10:12:06 +0200

    subiendo fragment03

---
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment03/Fragment03.java
@@ -0,0 +1,13 @@
+package com.iesvirgendelcarmen.dam.fragment03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragment03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment03);
+    }
+}
